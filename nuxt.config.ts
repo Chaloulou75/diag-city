4c6936b7c3e166e084697b44e9cb251aad9e02ec
Chaloulou75 <c.jeandey@gmail.com>
2024-05-13 21:58:54 +0200

    mise en place

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-})
+  plugins: ["~/plugins/preline.client.ts"],
+  modules: ["@nuxt/image", "@nuxtjs/seo"],
+  site: {
+    url: "https://diag-city.com",
+    name: "Diag City",
+    description: "Diag City!",
+    defaultLocale: "fr",
+  },
+});
