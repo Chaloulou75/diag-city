f00f7a7f200f6c37a90ec08039572d9c52b5ee9a
Chaloulou75 <c.jeandey@gmail.com>
2024-06-19 14:07:48 +0200

    hero

--- components/HeroImage.vue
+++ components/HeroImage.vue
@@ -4,28 +4,20 @@
     class="relative flex h-[30rem] flex-col items-center justify-between bg-gray-900 bg-opacity-90 bg-bagatelle bg-cover bg-center bg-no-repeat md:h-screen md:max-h-screen md:min-h-screen md:flex-row md:justify-evenly"
   >
     <div
-      class="flex flex-col items-center justify-center px-4 mx-auto basis-full md:mt-auto md:basis-1/2 md:px-8 md:pb-16 md:pt-8"
+      class="mx-auto flex basis-full flex-col items-center justify-center px-4 md:mt-auto md:basis-1/2 md:px-8 md:pb-16 md:pt-8"
     >
       <div
-        class="block text-5xl font-black tracking-wide text-center text-white md:text-left md:text-7xl"
+        class="block text-center text-5xl font-black tracking-wide text-white md:text-left md:text-7xl"
       >
         <span
-          class="text-transparent bg-gradient-to-b from-black to-diagcity-blue bg-clip-text md:bg-gradient-to-r md:from-white/90 md:to-white"
+          class="bg-gradient-to-b from-black to-diagcity-blue bg-clip-text text-transparent md:bg-gradient-to-r md:from-white/90 md:to-white"
           >Pour votre Copropriété</span
         >
       </div>
-      <!-- <div class="mt-6 md:mt-8 md:self-start">
-        <NuxtLink
-          to="/diagnostic/diagnostic-dtg"
-          class="px-4 py-3 text-xs font-semibold tracking-wide text-white rounded-md shadow-sm bg-diagcity-blue hover:bg-diagcity-violet focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-diagcity-blue md:text-sm"
-        >
-          En savoir plus
-        </NuxtLink>
-      </div> -->
     </div>
     <!-- mobile -->
     <div
-      class="grid h-full grid-cols-2 gap-4 px-4 py-4 place-content-end place-items-stretch md:hidden"
+      class="grid h-full grid-cols-2 place-content-end place-items-stretch gap-4 px-4 py-4 md:hidden"
     >
       <CardDiagComponent :title="'D.T.G'" :url="'/diagnostic/diagnostic-dtg'" />
       <!-- :logo="'/dtg.png'" -->
@@ -41,7 +33,7 @@
     </div>
     <!-- desktop -->
     <div
-      class="hidden h-full py-4 place-content-end place-items-stretch md:grid md:grid-cols-2 md:gap-8 md:px-8 md:py-8"
+      class="hidden h-full place-content-end place-items-stretch py-4 md:grid md:grid-cols-2 md:gap-8 md:px-8 md:py-8"
     >
       <CardDiagComponent
         :title="'Diagnostic Technique Global'"
--- pages/presentation.vue
+++ pages/presentation.vue
@@ -5,7 +5,7 @@
       :title="'Qui sommes nous?'"
       :image="'bg-gradient-to-r from-blue-500 to-diagcity-blue bg-opacity-20'"
     />
-    <div class="container px-4 mx-auto tracking-wider md:px-12">
+    <div class="container mx-auto px-4 tracking-wider md:px-12">
       <div class="space-y-6">
         <p class="text-lg leading-relaxed text-diagcity-gray md:text-xl">
           La société
@@ -18,8 +18,8 @@
         </p>
 
         <p class="text-lg leading-relaxed text-diagcity-gray md:text-xl">
-          Pour cela, nous vous garantissons des intervention justifiées et
-          rapides, à des prix honnétes, des technitiens certifiés et compétents,
+          Pour cela, nous vous garantissons des interventions justifiées et
+          rapides, à des prix honnétes, des techniciens certifiés et compétents,
           des conseils avisés et des rapports clairs et lisibles.
         </p>
         <h2 class="text-xl font-semibold text-diagcity-violet md:text-2xl">
@@ -28,12 +28,16 @@
         <p class="text-lg leading-relaxed text-diagcity-gray md:text-xl">
           Nos prestations incluent l’ensemble des équipements nécessaires à
           l’achèvement de la mission. <br />
-          Le cabinet DIAG CITY possède le matériel et les qualifications pour le
-          travail en hauteur ou d’accès (échelle de 3m), et l’outillage
-          spécifique à la réalisation des PPPT et des diagnostics réglementaires
-          (poinçon (termites/état parasitaire) EPI amiante, laser metre Leica,
-          vitromettre, humidimettre, tablette PC surface pro, analyseur pour la
-          présence de plomb ainsi qu’une flotte de vehicules…).
+          Le cabinet
+          <span class="font-semibold uppercase text-diagcity-violet"
+            >DIAG CITY</span
+          >
+          possède le matériel et les qualifications pour le travail en hauteur
+          ou d’accès (échelle de 3m), et l’outillage spécifique à la réalisation
+          des PPPT et des diagnostics réglementaires (poinçon (termites/état
+          parasitaire) EPI amiante, laser metre Leica, vitromettre,
+          humidimettre, tablette PC surface pro, analyseur pour la présence de
+          plomb ainsi qu’une flotte de vehicules…).
         </p>
         <h2 class="text-xl font-semibold text-diagcity-violet md:text-2xl">
           Diagnostiqueurs complémentaires
@@ -41,7 +45,7 @@
         <p class="text-lg leading-relaxed text-diagcity-gray md:text-xl">
           En cas d’urgence ou de réalisation de diagnostic urgent, nous serons
           en mesure de compléter notre équipe de diagnostiqueurs en faisant
-          appel à d’autre experts (plus de 30 techniciens) qui utilisent les
+          appel à d’autres experts (plus de 30 techniciens) qui utilisent les
           mêmes méthodes de travail et avec qui nous avons l’habitude de
           collaborer.
         </p>
