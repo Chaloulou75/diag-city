cd6db941f09473f52f5708bf8adb66e5dd3398b1
Chaloulou75 <c.jeandey@gmail.com>
2024-07-02 18:24:50 +0200

    googla analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,9 +17,15 @@ export default defineNuxtConfig({
     },
   },
   plugins: ["~/plugins/preline.client.ts"],
-  modules: ["@nuxt/image", "@nuxtjs/seo", "@nuxt/ui", "@vite-pwa/nuxt"],
+  modules: [
+    "@nuxt/image",
+    "@nuxtjs/seo",
+    "@nuxt/ui",
+    "@vite-pwa/nuxt",
+    "nuxt-gtag",
+  ],
   site: {
-    url: "https://diag-city.netlify.com",
+    url: "https://www.diag-city.fr",
     name: "Diag City - Diagnostics pour votre copropriété, Experts en Diagnostics Immobiliers, Amiante, Plomb, DPE, loi carrez, PPPT, DTG",
     description:
       "Diag City. Diagnostics pour votre copropriété. Diagnostics Immobiliers. Diagnostics vente. Diagnostics location. Diagnostics Amiantes. PPPT. Diagnostics électrique. Diagnostics Loi Carrez. Loi Boutin. Diagnostics D.P.E. Diagnostics Termites, Diagnostics Gaz. Diagnostics Plomb. Diagnostics E.R.P",
@@ -27,5 +33,8 @@ export default defineNuxtConfig({
   },
   image: {
     format: ["webp"],
+  },
+  gtag: {
+    id: "G-3J9T3R3BJ3",
   },
 });
